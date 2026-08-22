9d709813daa62828cdcbcba02c71f169538d3330
Shiva <shiva@example.com>
2026-08-22 23:41:35 +0530

    cors issue fixed

--- backend/app.py
+++ backend/app.py
@@ -92,7 +92,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://voice-ai-agent-cl3e.vercel.app",
+        "https://voice-ai-agent-ywov.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
